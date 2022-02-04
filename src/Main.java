e21ee9d8bb63eb69f7552c5fc3eb46e72b288932
Александр <taxinirvanaa@yandex.ru>
2022-02-04 20:52:26 +0500

    tiket

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+
+    public static void main(String[] args) {
+        //входные данные
+
+        int ticket = 330;
+        int amount = 20;
+
+
+        //логика
+        int miles = ticket / amount;
+
+        System.out.println(miles + " миль");
+    }
 }
